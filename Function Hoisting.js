82d2b4ce644ce1abbcb5437f080420e7738a5ffa
Soumyadeep Dey <161801065+Soumyadeep-Dey10@users.noreply.github.com>
2025-08-29 23:19:37 +0530

    Add files via upload

---
+++ Function Hoisting.js
@@ -0,0 +1,16 @@
+add()
+
+function add(){
+    let a = 10
+    b = 20 //as no keyword defined, it will act as var
+    console.log(a + b);
+}
+
+function add2(){
+    console.log("Hello");
+    
+}
+add2()
+console.log(add2);
+
+
